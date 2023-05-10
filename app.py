9ef8f95eb070cc47ab58de9c38ab9f11c227a54b
harishande24 <harish241198@gmail.com>
2023-05-09 22:51:55 -0400

    Updated Modules

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-
+from housing.logger import logging
+import sys
+from housing.exception import HousingException
 app=Flask(__name__ )
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
+    try:
+        raise Exception("We are testing Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+    logging.info(housing.error_message)    
+    logging.info("Testing Logging module")
     return " Starting ML Project"
 
 if __name__ == '__main__':
